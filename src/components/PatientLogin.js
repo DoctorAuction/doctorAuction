f8b6b4bc068db40778f413e06dad2dcaae2ca9af
谷口翔太 <taniguchishouta@taniguchishoutanoMacBook-Pro.local>
2021-02-17 14:07:08 +0900

    Created form page and the get the patient data

--- src/components/PatientLogin.js
+++ src/components/PatientLogin.js
@@ -1,33 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { Button } from "react-bootstrap";
 import "./Login.css";
 
 const PatientLogin = () => {
-  const handleLogin = () => {
-    console.log("shgota");
-    return;
+  const [symptom, setSymptom] = useState("");
+  const [money, setMoney] = useState("");
+
+  const handleSetSympton = (e) => {
+    setSymptom(e.target.value);
+    console.log(symptom);
   };
+
+  const handleSetMoney = (e) => {
+    setMoney(e.target.value);
+    console.log(money);
+  };
+
+  const patientData = () => {
+    const data = { symptom: symptom, money: money };
+    console.log(data);
+  };
+
   return (
     <section className="login">
       <div className="loginContainer">
         <h1 className="title">Consulting Form</h1>
         <label className="text">Symptom</label>
         <input
-          type="textarea"
+          type="text"
           autoFocus
           required
           placeholder="ex) backache"
           className="symptomform"
           wrap="hard"
+          onChange={handleSetSympton}
         />
         <p className="errorMsg"></p>
         <label>How much money do you pay?</label>
-        <input type="tel" required placeholder="ex) 2000" />
+        <input
+          type="tel"
+          required
+          placeholder="ex) 2000"
+          onChange={handleSetMoney}
+        />
         <p className="errorMsg"></p>
         <div className="btnContainer">
           <Link to="/doctor" className="btn1">
-            <Button variant="success" onClick={handleLogin}>
+            <Button variant="success" onClick={patientData}>
               Apply
             </Button>
           </Link>
