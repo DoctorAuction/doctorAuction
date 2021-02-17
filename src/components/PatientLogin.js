56aa888516df6d9540cb4077ec0c31218d8cc9f6
谷口翔太 <taniguchishouta@taniguchishoutanoMacBook-Pro.local>
2021-02-17 12:29:49 +0900

    Created consulting form

--- src/components/PatientLogin.js
+++ src/components/PatientLogin.js
@@ -1,15 +1,42 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { Button } from "react-bootstrap";
+import "./Login.css";
 
 const PatientLogin = () => {
+  const handleLogin = () => {
+    console.log("shgota");
+    return;
+  };
   return (
-    <>
-      <h1>Here is Patient Login page!!!</h1>
-      <Link to="/">
-        <Button variant="primary">Back to Home</Button>
-      </Link>
-    </>
+    <section className="login">
+      <div className="loginContainer">
+        <h1 className="title">Consulting Form</h1>
+        <label className="text">Symptom</label>
+        <input
+          type="textarea"
+          autoFocus
+          required
+          placeholder="ex) backache"
+          className="symptomform"
+          wrap="hard"
+        />
+        <p className="errorMsg"></p>
+        <label>How much money do you pay?</label>
+        <input type="tel" required placeholder="ex) 2000" />
+        <p className="errorMsg"></p>
+        <div className="btnContainer">
+          <Link to="/doctor" className="btn1">
+            <Button variant="success" onClick={handleLogin}>
+              Apply
+            </Button>
+          </Link>
+          <Link to="/" className="btn2">
+            <Button variant="primary">Back to Home</Button>
+          </Link>
+        </div>
+      </div>
+    </section>
   );
 };
 
