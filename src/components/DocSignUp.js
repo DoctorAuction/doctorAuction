04db70498d4cc27d9ef8ffe6ec9ff8610ea466e8
Yoko Tsuda <yoko20209@gmail.com>
2021-02-17 11:10:47 +0900

    used bootstrap

--- src/components/DocSignUp.js
+++ src/components/DocSignUp.js
@@ -1,5 +1,9 @@
 import { useState } from "react"
 import React from 'react';
+import Form from "react-bootstrap/Form";
+import FormControl from "react-bootstrap/FormControl";  
+import InputGroup from "react-bootstrap/InputGroup";
+
 
 function DocSignUp() {
   function handleSubmit(){
@@ -8,15 +12,34 @@ function DocSignUp() {
   
     return (
       <div className="DocSignUp">
-        <form id="doc_sign_up_form">
-            <a>Name</a>
+        <InputGroup id="doc_sign_up_InputGroup">
+          <InputGroup.prepend>
+            <InputGroup.Text >Name</InputGroup.Text>
+          </InputGroup.prepend>
+          <FormControl placeholder="Name" />
+  
+            {/* <a>Name</a>
             <input type="text" placeholder="Name"></input><br></br>
             <a>Graduation Year</a>
             <input type="text" placeholder="Graduation Year"></input><br></br>
             <a>Speciality</a>
             <input type="text" placeholder="Speciality"></input><br></br>
-            <input type="button" onClick={handleSubmit} value="Submit"></input>
-        </form>
+            <input type="button" onClick={handleSubmit} value="Submit"></input> */}
+        </InputGroup>
+
+        <InputGroup>
+          <InputGroup.prepend>
+              <InputGroup.Text >Graduation Year</InputGroup.Text>
+            </InputGroup.prepend>
+            <FormControl placeholder="Graduation Year" />
+        </InputGroup>
+
+        <InputGroup>
+          <InputGroup.prepend>
+          <InputGroup.Text >Graduation Year</InputGroup.Text>
+          </InputGroup.prepend>
+          <FormControl placeholder="Graduation Year" />
+        </InputGroup>
       </div>
     );
   }
